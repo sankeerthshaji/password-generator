a4c091508fa9045df1f003780ba9585ae4928cf2
Sankeerth Shaji <sankeerthsachu@gmail.com>
2023-06-23 12:15:01 +0530

    cors updated

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 //middleware
 app.use(cors({
-  origin:['https://passgenxapp.netlify.app/', 'http://localhost:3000']
+  origin:['https://passgenxapp.netlify.app', 'http://localhost:3000']
 }));
 app.use(bodyParser.json());
 
